38fadea788899f74ae5b07b48a1cecdc895529ed
Riya6567 <deyr13733@gmail.com>
2025-01-12 14:33:27 +0530

    Add files via upload

---
+++ Ass 4(d) To-Do List/Ass4d.js
@@ -0,0 +1,27 @@
+document.getElementById('addTaskButton').addEventListener('click', function(){
+    const taskInput = document.getElementById('taskInput');
+    const task = taskInput.value.trim();
+    if (task !== "")
+    {
+        addTask(task);
+        taskInput.value = "";
+    }
+});
+function addTask(task)
+{
+    const taskList = document.getElementById('taskList');
+    const li = document.createElement('li');
+    li.textContent = task;
+
+    const deleteButton = document.createElement('span');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete';
+
+    // Capture the li element in a closure
+    deleteButton.addEventListener('click', function() {
+        taskList.removeChild(li);
+    });
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+}
